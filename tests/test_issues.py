c13f9caf455ac185d78e143cb6b1d4722caabc5e
Brian Pepple <bdpepple@gmail.com>
2021-08-29 23:17:15 -0400

    Fix bug where an issue doesn't have a store_date (#3)

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 
 from Simyan import APIError
@@ -27,3 +29,15 @@ def test_issue_list(talker):
 def test_issue_list_empty(talker):
     results = talker.issue_list({'filter': 'name:INVALID'})
     assert len(results) == 0
+
+def test_issue_bad_cover_date(talker):
+    xmen_2 = talker.issue(6787)
+    assert xmen_2.store_date is None
+    assert xmen_2.cover_date == datetime.date(1963, 11, 1)
+    assert xmen_2.id == 6787
+    assert xmen_2.issue_number == "2"
+    assert len(xmen_2.person_credits) == 4
+    assert xmen_2.person_credits[0].name == "Jack Kirby"
+    assert xmen_2.person_credits[0].role == "penciler"
+    assert len(xmen_2.character_credits) == 10
+    assert xmen_2.character_credits[0].name == "Angel"
