4d2509f84256310e3f78e8250b712ec1846ffc15
Buried-In-Code <6057651+Buried-In-Code@users.noreply.github.com>
2021-09-08 15:17:08 +1200

    Removed has_staff_review

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,10 @@ from Simyan import api, sqlite_cache
 
 
 @pytest.fixture(scope="session")
-def api_key():
-    return os.getenv("COMICVINE", "INVALID")
+def comicvine_api_key():
+    return os.getenv("COMICVINE_API_KEY", "INVALID")
 
 
 @pytest.fixture(scope="session")
-def talker(api_key):
-    print(api_key)
-    return api(
-        api_key=api_key,
-        cache=sqlite_cache.SqliteCache("tests/Simyan-Cache.sqlite"),
-    )
+def comicvine(comicvine_api_key):
+    return api(api_key=comicvine_api_key, cache=sqlite_cache.SqliteCache("tests/Simyan-Cache.sqlite"))
