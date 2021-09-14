5a6016b3e20e493bf611ac978681276cbcf8ff5c
Brian Pepple <bdpepple@gmail.com>
2021-09-14 11:53:28 -0400

    Add some basic docstring to test configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+"""
+Conftest module.
+This module contains pytest fixtures.
+"""
 import os
 
 import pytest
@@ -7,9 +11,11 @@ from Simyan import api, sqlite_cache
 
 @pytest.fixture(scope="session")
 def comicvine_api_key():
+    """Comic Vine api key fixture."""
     return os.getenv("COMICVINE_API_KEY", "INVALID")
 
 
 @pytest.fixture(scope="session")
 def comicvine(comicvine_api_key):
+    """Simyan api fixture."""
     return api(api_key=comicvine_api_key, cache=sqlite_cache.SqliteCache("tests/Simyan-Cache.sqlite", expiry=None))
