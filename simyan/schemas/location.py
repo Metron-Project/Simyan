311b804dce8e93805c652e4dd6ad33942165a231
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2022-09-14 12:15:20 +1200

    Fixup Location docstrings

--- simyan/schemas/location.py
+++ simyan/schemas/location.py
@@ -74,19 +74,19 @@ class LocationEntry(BaseModel):
     The LocationEntry object contains information for a location.
 
     Attributes:
-        aliases: List of names used by the Location, separated by `~\r\n`.
+        aliases: List of names used by the LocationEntry, separated by `~\r\n`.
         api_url: Url to the resource in the Comicvine API.
-        date_added: Date and time when the Location was added.
-        date_last_updated: Date and time when the Location was last updated.
-        description: Long description of the Location.
-        first_issue: First issue the Location appeared in.
-        image: Different sized images, posters and thumbnails for the Location.
-        issue_count: Number of issues the Location appears in.
+        date_added: Date and time when the LocationEntry was added.
+        date_last_updated: Date and time when the LocationEntry was last updated.
+        description: Long description of the LocationEntry.
+        first_issue: First issue the LocationEntry appeared in.
+        image: Different sized images, posters and thumbnails for the LocationEntry.
+        issue_count: Number of issues the LocationEntry appears in.
         location_id: Identifier used by Comicvine.
-        name: Name/Title of the Location.
+        name: Name/Title of the LocationEntry.
         site_url: Url to the resource in Comicvine.
-        start_year: The year the Location was first used.
-        summary: Short description of the Location.
+        start_year: The year the LocationEntry was first used.
+        summary: Short description of the LocationEntry.
     """
 
     aliases: Optional[str] = None
@@ -106,7 +106,7 @@ class LocationEntry(BaseModel):
     @property
     def alias_list(self) -> List[str]:
         r"""
-        List of aliases the Location has used.
+        List of aliases the LocationEntry has used.
 
         Returns:
             List of aliases, split by `~\r\n`
