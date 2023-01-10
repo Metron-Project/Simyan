040e8dff9fb9d099eeee5c6dae12159c385cd7d6
Buried-In-Code <6057651+Buried-In-Code@users.noreply.github.com>
2023-01-10 16:54:08 +1300

    XDG Support (#159)
    - Update `get_config_root()` to support XDG_CONFIG_HOME
    - Version bump

--- simyan/__init__.py
+++ simyan/__init__.py
@@ -1,17 +1,19 @@
 """simyan package entry file."""
-__version__ = "0.12.0"
+__version__ = "0.13.0"
 __all__ = ["__version__", "get_cache_root"]
 
+import os
 from pathlib import Path
 
 
 def get_cache_root() -> Path:
     """
-    Create and return the path to the cache for simyan.
+    Create and return the path to the cache for simyan, supports XDG_CACHE_HOME env.
 
     Returns:
-        The path to the simyan cache
+        The path to the simyan cache folder.
     """
-    folder = Path.home() / ".cache" / "simyan"
+    cache_home = os.getenv("XDG_CACHE_HOME", default=str(Path.home() / ".cache"))
+    folder = Path(cache_home).resolve() / "simyan"
     folder.mkdir(parents=True, exist_ok=True)
     return folder
