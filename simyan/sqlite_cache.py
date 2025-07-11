d23612480e87373e49f43318f93d6b51ba54711a
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2025-07-11 23:34:16 +1200

    Use contextmanager for sqlite connection

--- simyan/sqlite_cache.py
+++ simyan/sqlite_cache.py
@@ -23,11 +23,27 @@ class SQLiteCache:
     """
 
     def __init__(self, path: Optional[Path] = None, expiry: Optional[int] = 14):
-        self.expiry = expiry
-        self.connection = sqlite3.connect(path or get_cache_root() / "cache.sqlite")
-        self.connection.row_factory = sqlite3.Row
+        self._db_path = path or (get_cache_root() / "cache.sqlite")
+        self._expiry = expiry
+        self.initialize()
 
-        self.connection.execute("CREATE TABLE IF NOT EXISTS queries (query, response, query_date);")
+    def _connect(self) -> sqlite3.Connection:
+        conn = sqlite3.connect(self._db_path)
+        conn.row_factory = sqlite3.Row
+        conn.execute("PRAGMA foreign_keys = ON")
+        return conn
+
+    def initialize(self) -> None:
+        with self._connect() as conn:
+            conn.execute(
+                """
+                CREATE TABLE IF NOT EXISTS queries (
+                    query TEXT NOT NULL PRIMARY KEY,
+                    response TEXT,
+                    query_date DATE
+                );
+                """
+            )
         self.cleanup()
 
     def select(self, query: str) -> dict[str, Any]:
@@ -39,17 +55,18 @@ class SQLiteCache:
         Returns:
             Empty dict or select results.
         """
-        if self.expiry:
-            expiry = datetime.now(tz=timezone.utc).astimezone().date() - timedelta(days=self.expiry)
-            cursor = self.connection.execute(
-                "SELECT * FROM queries WHERE query = ? and query_date > ?;",
-                (query, expiry.isoformat()),
-            )
-        else:
-            cursor = self.connection.execute("SELECT * FROM queries WHERE query = ?;", (query,))
-        if results := cursor.fetchone():
-            return json.loads(results["response"])
-        return {}
+        with self._connect() as conn:
+            if self._expiry:
+                expiry = datetime.now(tz=timezone.utc).astimezone().date() - timedelta(
+                    days=self._expiry
+                )
+                row = conn.execute(
+                    "SELECT * FROM queries WHERE query = ? and query_date > ?;",
+                    (query, expiry.isoformat()),
+                ).fetchone()
+            else:
+                row = conn.execute("SELECT * FROM queries WHERE query = ?;", (query,)).fetchone()
+            return json.loads(row["response"]) if row else {}
 
     def insert(self, query: str, response: dict[str, Any]) -> None:
         """Insert data into the cache database.
@@ -58,15 +75,15 @@ class SQLiteCache:
             query: Url string used as key.
             response: Response dict from url.
         """
-        self.connection.execute(
-            "INSERT INTO queries (query, response, query_date) VALUES (?, ?, ?);",
-            (
-                query,
-                json.dumps(response),
-                datetime.now(tz=timezone.utc).astimezone().date().isoformat(),
-            ),
-        )
-        self.connection.commit()
+        with self._connect() as conn:
+            conn.execute(
+                "INSERT INTO queries (query, response, query_date) VALUES (?, ?, ?);",
+                (
+                    query,
+                    json.dumps(response),
+                    datetime.now(tz=timezone.utc).astimezone().date().isoformat(),
+                ),
+            )
 
     def delete(self, query: str) -> None:
         """Remove entry from the cache with the provided url.
@@ -74,13 +91,13 @@ class SQLiteCache:
         Args:
           query: Url string used as key.
         """
-        self.connection.execute("DELETE FROM queries WHERE query = ?;", (query,))
-        self.connection.commit()
+        with self._connect() as conn:
+            conn.execute("DELETE FROM queries WHERE query = ?;", (query,))
 
     def cleanup(self) -> None:
         """Remove all expired entries from the cache database."""
-        if not self.expiry:
+        if not self._expiry:
             return
-        expiry = datetime.now(tz=timezone.utc).astimezone().date() - timedelta(days=self.expiry)
-        self.connection.execute("DELETE FROM queries WHERE query_date < ?;", (expiry.isoformat(),))
-        self.connection.commit()
+        expiry = datetime.now(tz=timezone.utc).astimezone().date() - timedelta(days=self._expiry)
+        with self._connect() as conn:
+            conn.execute("DELETE FROM queries WHERE query_date < ?;", (expiry.isoformat(),))
